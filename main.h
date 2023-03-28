5a9492c51760255c60f67bff8f7edeb9c4c9556f
Emilio Damasco <6338@holbertonstudents.com>
2023-03-28 06:57:30 -0700

    added get_func prototype to main.h

--- main.h
+++ main.h
@@ -6,12 +6,13 @@ int _putchar(char c);
 int print_c(va_list c);
 int print_s(va_list s);
 int print_d(va_list n);
+int (*get_func(char *s))(va_list);
 
 typedef struct format
 {
 	char *format;
 	int (*f)(va_list);
 	int r = f;
-}
+} format;
 
 #endif
