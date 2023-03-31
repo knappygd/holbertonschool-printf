bd5822b4f8e01f40b15e7e9dda2053b04a3145e7
Emilio Damasco <6338@holbertonstudents.com>
2023-03-31 10:16:24 -0700

    added new advanced function prototypes

--- main.h
+++ main.h
@@ -4,13 +4,23 @@
 #include <unistd.h>
 
 int _printf(const char *format, ...);
-int _putchar(char c);
-int print_c(va_list c);
-int print_s(va_list s);
-int print_d(va_list n);
-int (*get_func(char s))(va_list);
-int reco_array(char c);
-int _abs(int n);
+int _putchar(char);
+int print_c(va_list);
+int print_s(va_list);
+int print_d(va_list);
+int (*get_func(char))(va_list);
+int reco_array(char);
+int _abs(int);
+int reverse(va_list);
+int decimal(va_list);
+int rot_13(va_list);
+int hex(va_list);
+int x_to_upper(va_list);
+int print_per(va_list);
+int bin(va_list);
+int baseconv(unsigned int, unsigned int);
+int eq_pos(int);
+int d(unsigned int);
 
 /**
  * struct format - data of functions
